9230665e73d97a9b60744660aad2ecbc15256169
vanthoang <hoangthoang0309@gmail.com>
2023-04-02 00:02:00 +0700

    create home page

--- src/app/containers/home-page/HomePage.tsx
+++ src/app/containers/home-page/HomePage.tsx
@@ -1,5 +1,9 @@
 import LoGo from "@/app/components/Icon/LoGo"
 import UpLoadIcon from "@/app/components/Icon/UpLoadIcon"
+import Image from 'next/image'
+import profilePic from '../../images/banner.svg'
+import MergeIcon from "@/app/components/Icon/MergeIcon"
+import FileIcon from "@/app/components/Icon/FileIcon"
 
 export const HomePage = () => {
     return (
@@ -8,15 +12,54 @@ export const HomePage = () => {
                 <div className="flex justify-between items-center">
                     <div className="flex items-center">
                         <LoGo />
-                        <p className="font-bold text-3xl">PDF TOOLS</p>
+                        <p className="font-bold text-4xl ml-5">PDF TOOLS</p>
                     </div>
                     <div>
                         <button type="button"
-                            className="flex items-center rounded-full bg-gradient-to-r from-[#2259C9] from-[#00BAEB]">
+                            className="flex items-center rounded-full  bg-gradient-to-r to-cyan-500 from-blue-500 px-7 py-3">
                             <UpLoadIcon />
-                            Upload File To Convert PDF
+                            <p className="ml-3.5 font-bold text-lg text-white">Upload File To Convert PDF</p>
                         </button>
                     </div>
+                </div>
+            </div>
+            <div className="grid grid-cols-2 gap-x-8 items-center pt-5">
+                <div>
+                    <h1 className="text-[65px] font-bold">We make PDF easy.</h1>
+                    <p className="text-base">All the tools you’ll need to be more productive and work smarter with documents.</p>
+                </div>
+                <div>
+                    <Image
+                        src={profilePic}
+                        alt="Picture of the author"
+                    />
+                </div>
+            </div>
+            <div className="text-center mt-20">
+                <h1 className="text-[65px] font-bold">Most Popular PDF Tools</h1>
+            </div>
+            <div className="grid grid-cols-2 gap-x-8 mt-14 mb-20">
+                <div className="shadow-[0px_4px_24px_0px_rgba(0,0,0,0.1)] text-center p-12">
+                    <div className="flex items-center justify-center mb-[18px]">
+                        <MergeIcon />
+                        <p className="font-bold ml-8">Merge PDF</p>
+                    </div>
+                    <p>Combine multiple PDFs into one unified document</p>
+                </div>
+                <div className="shadow-[0px_4px_24px_0px_rgba(0,0,0,0.1)] text-center p-12">
+                    <div className="flex items-center justify-center mb-[18px]">
+                        <FileIcon />
+                        <p className="font-bold ml-8">Convert PDF</p>
+                    </div>
+                    <p>Convert image to editable PDF</p>
+                </div>
+            </div>
+            <div className="grid grid-cols-2 bg-[#ECF3FE] p-7 text-[#2259C9] text-2xl">
+                <p className="font-bold">© 2022 Alright services</p>
+                <div className="flex justify-between font-bold">
+                    <p>Privacy Notice</p>
+                    <p>Term & Condition</p>
+                    <p>Contact Us</p>
                 </div>
             </div>
         </>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { HomePage } from "./containers/home-page/HomePage";
 
 export default function Page() {
   return (
-    <div className="max-w-screen-lg mx-auto">
+    <div className="max-w-screen-xl mx-auto">
       <HomePage />
     </div>
   )
