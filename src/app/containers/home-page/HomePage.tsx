c2fc9f5895e545c034ce3886aed2646da2a5895b
vanthoang <hoangthoang0309@gmail.com>
2023-04-07 21:51:29 +0700

    fix bg

--- src/app/containers/home-page/HomePage.tsx
+++ src/app/containers/home-page/HomePage.tsx
@@ -12,7 +12,6 @@ import EditIcon from "@/app/components/Icon/EditIcon";
 import DownLoadIconIcon from "@/app/components/Icon/DownLoadIcon";
 import ViewIcon from "@/app/components/Icon/ViewIcon";
 import RestartIcon from "@/app/components/Icon/RestartIcon";
-import clsx from 'clsx';
 
 export const HomePage = () => {
     const [isMerge, setIsMerge] = useState(false)
@@ -49,14 +48,14 @@ export const HomePage = () => {
                 <h1 className="text-[65px] font-bold">Most Popular PDF Tools</h1>
             </div>
             {!isMerge && (<div className="grid grid-cols-2 gap-x-8 mt-14 mb-20">
-                <div onClick={() => setIsMerge(true)} className="shadow-[0px_4px_24px_0px_rgba(0,0,0,0.1)] text-center p-12 cursor-pointer">
+                <div onClick={() => setIsMerge(true)} className="shadow-[0px_4px_24px_0px_rgba(0,0,0,0.1)] text-center p-12 cursor-pointer bg-white">
                     <div className="flex items-center justify-center mb-[18px]">
                         <MergeIcon />
                         <p className="font-bold ml-8 text-2xl">Merge PDF</p>
                     </div>
                     <p className="text-gray-500 text-lg">Combine multiple PDFs into one unified document</p>
                 </div>
-                <div className="shadow-[0px_4px_24px_0px_rgba(0,0,0,0.1)] text-center p-12 cursor-pointer">
+                <div onClick={() => setIsMerge(true)} className="shadow-[0px_4px_24px_0px_rgba(0,0,0,0.1)] text-center p-12 cursor-pointer bg-white">
                     <div className="flex items-center justify-center mb-[18px]">
                         <FileIcon />
                         <p className="font-bold ml-8 text-2xl">Convert PDF</p>
@@ -66,36 +65,36 @@ export const HomePage = () => {
             </div >)
             }
             {isMerge && (
-           <>
-                <MergePDF />
-                <div className="flex justify-center items-center mb-[110px]">
-                   <div className="text-center">
-                        <p className="font-[600] text-2xl">PDF has merged your files into a single PDF</p>
-                        <div className="flex justify-center border border-dashed border-sky-600 p-4 my-4">
-                            <EditIcon />
-                            <p className="text-[#2259C9] font-bold text-lg">
-                                Loremipsumissimplytext_merged.pdf - 222.5KB
-                            </p>
+                <>
+                    <MergePDF />
+                    <div className="flex justify-center items-center mb-[110px]">
+                        <div className="text-center">
+                            <p className="font-[600] text-2xl">PDF has merged your files into a single PDF</p>
+                            <div className="flex justify-center border border-dashed border-sky-600 p-4 my-4">
+                                <EditIcon />
+                                <p className="text-[#2259C9] font-bold text-lg">
+                                    Loremipsumissimplytext_merged.pdf - 222.5KB
+                                </p>
+                            </div>
+                            <div className="flex items-center justify-center gap-4">
+                                <button className="flex items-center min-w-[154px] bg-[#2259C9] justify-center rounded-full px-4 py-2 gap-2 text-white font-bold">
+                                    <DownLoadIconIcon />
+                                    Download
+                                </button>
+                                <button className="flex items-center min-w-[154px] rounded-full bg-[#22C96F] justify-center px-4 py-2 gap-2 text-white font-bold">
+                                    <ViewIcon />
+                                    Preview
+                                </button>
+                                <button className="flex items-center min-w-[154px] rounded-full bg-[#C92268] justify-center px-4 py-2 gap-2 text-white font-bold">
+                                    <RestartIcon />
+                                    Restart
+                                </button>
+                            </div>
                         </div>
-                        <div className="flex items-center justify-center gap-4">
-                            <button className="flex items-center min-w-[154px] bg-[#2259C9] justify-center rounded-full px-4 py-2 gap-2 text-white font-bold">
-                                <DownLoadIconIcon/>
-                                Download
-                            </button>
-                            <button className="flex items-center min-w-[154px] rounded-full bg-[#22C96F] justify-center px-4 py-2 gap-2 text-white font-bold">
-                                <ViewIcon/>
-                                Preview
-                            </button>
-                            <button className="flex items-center min-w-[154px] rounded-full bg-[#C92268] justify-center px-4 py-2 gap-2 text-white font-bold">
-                                <RestartIcon/>
-                                Restart 
-                            </button>
-                        </div>
-                   </div>
-                </div>
-           </>
+                    </div>
+                </>
             )}
-            
+
         </>
     )
 }
